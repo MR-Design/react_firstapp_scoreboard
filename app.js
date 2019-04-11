d7e9c412c1b9691e860c6efc59752a7164ed21e3
MR-Design <two7city@gmail.com>
2019-04-10 23:38:07 -0500

    Composint Components

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header(){
+const Header = () => {
     return(
         <header>
             <h1> Scoreboard</h1>
@@ -6,8 +6,34 @@ function Header(){
         </header>
     );
 }
+const Player =() => {
+    return(
+        <div className="player">
+        <span className="player-name"> Moh </span>
+        <Counter/>
+        </div>
+    );
+}
+const Counter =()=>{
+    return(
+      <div className="counter">
+         <button className="counter-action decrement"> - </button>
+         <span className="counter-score">35</span>
+         <button className="counter-action increment"> + </button>
+      </div>
+    );
+}
+const App =()=>{
+    return(
+      <div className="scoreboard">
+         <Header/>
 
+         {/*  Player List */}
+         <Player/>
+      </div>
+    );
+}
 ReactDOM.render(
-    <Header />,
+    <App />,
     document.getElementById('root')
 );
