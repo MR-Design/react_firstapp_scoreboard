beaa9b0482c5548e6b731bb96ab3394b0cfc9c3f
MR-Design <two7city@gmail.com>
2019-04-15 15:09:31 -0500

    Create a Component as a Class

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const players=[
     {
-        name: "Guil",
+        name: "Mohammed",
         score: 50,
         id:1
       },
       {
-        name: "Treasure",
+        name: "Reda",
         score: 85,
         id:2
       },
       {
-        name: "Ashley",
+        name: "Wassila",
         score: 95,
         id:3
       },
       {
-        name: "James",
+        name: "Fatiha",
         score: 80,
         id:4
       }
@@ -39,14 +39,18 @@ const Player =(props) => {
         </div>
     );
 }
-const Counter =(props)=>{
-    return(
-      <div className="counter">
-         <button className="counter-action decrement"> - </button>
-         <span className="counter-score">{props.score}</span>
-         <button className="counter-action increment"> + </button>
-      </div>
-    );
+class Counter extends  React.Component{
+    render(){
+        return(
+            <div className="counter">
+               <button className="counter-action decrement"> - </button>
+               <span className="counter-score">{this.props.score}</span>
+               <button className="counter-action increment"> + </button>
+            </div>
+          );
+
+    }
+  
 }
 const App =(props)=>{
     return(
