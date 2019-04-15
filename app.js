59b79768effd57037dab064a8fc17bfb69307682
MR-Design <two7city@gmail.com>
2019-04-15 15:58:37 -0500

    Bind Event Handlers to Components incremnet and decrement Score

--- app.js
+++ app.js
@@ -33,19 +33,34 @@ const Player =(props) => {
     return(
         <div className="player">
         <span className="player-name"> {props.name} </span>
-        <Counter
-            score={props.score}
-        />
+        <Counter/>
         </div>
     );
 }
-class Counter extends  React.Component{
+class Counter extends React.Component {
+    state = {
+        score :0
+    };
+
+    incrementScore =() =>{
+        this.setState({
+            score: this.state.score +1
+        });
+    }
+    decrementScore =() =>{
+        this.setState({
+            score: this.state.score -1
+        });
+    }
+
+
+
     render(){
         return(
             <div className="counter">
-               <button className="counter-action decrement"> - </button>
-               <span className="counter-score">{this.props.score}</span>
-               <button className="counter-action increment"> + </button>
+               <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+               <span className="counter-score">{this.state.score}</span>
+               <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
           );
 
@@ -64,7 +79,6 @@ const App =(props)=>{
          {props.initialPlayers.map( player => 
             <Player 
             name={player.name} 
-            score={player.score}
             key ={player.id.toString()}
             />
 
